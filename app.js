d0641c311cc8899a23f343e98d96f436e0b76c98
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-15 10:56:29 +0300

    Code starts simple It gets complex and becomes difficult to handle at some point It contains to many uses cases to keep in our heads

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const someOrder = {
   items: [
-    { name: 'oranges', price: 8 },
-    { name: 'avacados', price: 5 },
-    { name: 'mango', price: 5 },
-    { name: 'bananas', price: 2 },
+    { name: 'oranges', price: 8, quantity: 10 },
+    { name: 'avacados', price: 5, quantity: 1 },
+    { name: 'mango', price: 5, quantity: 2 },
+    { name: 'bananas', price: 2, quantity: 10 },
+    { name: 'shipping', price: 40, shipping: true },
   ],
 };
 
-const orderTotal = order => order.items.reduce((prev, cur) => prev + cur.price, 0)
+const orderTotal = (order) => {
+  const totalItems = order.items
+    .filter((x) => !x.shipping)
+    .reduce((prev, cur) => prev + cur.price * cur.quantity, 0);
+
+    const shippingItem = order.items.find( x => !!x.shipping)
+    const shipping = totalItems > 1000 ? 0 : shippingItem.price;
+    return totalItems + shipping
+};
 
 console.log(orderTotal(someOrder));
