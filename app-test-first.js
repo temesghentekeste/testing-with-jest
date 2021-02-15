ad3c53366ad8c3120e6a9d39c7a04756c9a26418
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-15 11:31:22 +0300

    Three approaches to write a test when writing a unit test   - obvious implementation   - fake it until you make it   - triangulation       - triangulation: we write extra test and our code fails, then we write the implementation       - it is safe and looks hard to make mistakes with it       - it has the quality of making it exactly do what you need it to do       - focus on what you need to write to pass the test and add more test

--- app-test-first.js
+++ app-test-first.js
@@ -3,9 +3,22 @@ if( orderTotal({
     { name: 'oranges', price: 8, quantity: 10 },
     { name: 'avacados', price: 5, quantity: 1 },
   ]}) !== 85)  {
-    throw new Error( 'Check fail')
+    throw new Error( 'Check fail: Happy path(1)')
   }
 
-  function orderTotal() {
-    return 85; // at this point the test passes
+  if (
+    orderTotal({
+      items: [
+        { name: 'bananas', price: 10, quantity: 10 },
+        { name: 'apple', price: 15, quantity: 20 },
+      ],
+    }) !== 400
+  ) {
+    throw new Error('Check fail: Happy path(2)');
+  }
+
+  function orderTotal(order) {
+    const total = order.items.reduce( (prev, cur) => prev + cur.price * cur.quantity, 0)
+    console.log(total);
+    return total;
   }
