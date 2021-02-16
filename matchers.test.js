2637c13d831a8ec19a1ef7d72a9e4cf54179895e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-16 17:56:57 +0300

    Exceptions

--- matchers.test.js
+++ matchers.test.js
@@ -90,3 +90,19 @@ test('the shopping list has milk on it', () => {
   expect(shoppingList).not.toContain('coke');
   expect(new Set(shoppingList)).toContain('milk');
 });
+
+// Exceptions
+// If you want to test whether a particular function throws an error when it's called, use toThrow.
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
