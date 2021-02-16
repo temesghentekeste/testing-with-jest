c7ae095ee0f849ba96434d146767b708100442ee
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-16 17:44:12 +0300

    Numbers

--- matchers.test.js
+++ matchers.test.js
@@ -41,3 +41,18 @@ test('zero', () => {
   expect(z).toBeFalsy();
 });
 
+// Numbers
+// Most ways of comparing numbers have matcher equivalents.
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+
