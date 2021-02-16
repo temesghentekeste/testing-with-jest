33a9fd63fba4d6e7dd1cf2433b4213dedecc4a87
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-16 17:39:56 +0300

    Common matchers

--- order-total.test.js
+++ order-total.test.js
@@ -36,3 +36,10 @@ it('Without Quantity', () => {
   ).toBe(25);
 });
 
+
+
+test('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
