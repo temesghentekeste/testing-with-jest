b9b907a72f0cd4e218063c0cd6689b6a4acc4683
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-16 17:43:43 +0300

    Truthiness

--- matchers.test.js
+++ matchers.test.js
@@ -1,3 +1,4 @@
+// Common matchers
 test('object assignment', () => {
   const data = { one: 1 };
   data['two'] = 2;
@@ -11,3 +12,32 @@ test('adding positive numbers is not zero', () => {
     }
   }
 });
+
+// Truthiness
+// In tests, you sometimes need to distinguish between undefined, null, and false, but you sometimes do not want to treat these differently. Jest contains helpers that let you be explicit about what you want.
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+// For example:
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
