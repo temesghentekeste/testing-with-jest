1752d6fb1f8bdce3c8b529a2b1964571eb4b5f41
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-16 17:45:07 +0300

    Floating point

--- matchers.test.js
+++ matchers.test.js
@@ -56,3 +56,9 @@ test('two plus two', () => {
   expect(value).toEqual(4);
 });
 
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
